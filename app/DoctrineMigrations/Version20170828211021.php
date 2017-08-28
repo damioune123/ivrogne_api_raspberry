3b80d77fcc0ed709fafed5d5e8913701fb2d6f39
damioune123 <damienmeur@gmail.com>
2017-08-29 00:00:45 +0200

    version migration

---
+++ app/DoctrineMigrations/Version20170828211021.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170828211021 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE products ADD product_promotion_user DOUBLE PRECISION NOT NULL, CHANGE product_promotion product_promotion_admin DOUBLE PRECISION NOT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE products ADD product_promotion DOUBLE PRECISION NOT NULL, DROP product_promotion_admin, DROP product_promotion_user');
+    }
+}
